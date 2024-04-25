1b4da21bb73216a3ae4b43a1ed840fc5fa5ebf97
Paulo dos Santos <paulo.dossantos@hotmail.com>
2024-04-25 17:34:02 -0300

    fix: change build.gradle get

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ fun tryLocalProperties() =
     project.rootProject.file(Dependencies.Commons.localProperties).inputStream()
 
 fun tryPropertiesFromGitHub() =
-    File("./local.properties").absoluteFile.inputStream()
+    File(Dependencies.Commons.localProperties).absoluteFile.inputStream()
 
 fun updateFirebaseApi(key: String) {
     val jsonFilePath = Dependencies.Commons.googleServicesJsonPath

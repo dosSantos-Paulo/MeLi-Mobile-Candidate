84975758a72b7a27453012ac3111e0a134e3b02d
Paulo dos Santos <paulo.dossantos@hotmail.com>
2024-04-26 17:24:47 -0300

    workflow test: try to uses gradle cache

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,71 @@ plugins {
     id(Plugins.kotlinAndroid) version Versions.Core.kotlin apply false
     id(Plugins.googleServices) version Versions.Google.googleServices apply false
     id(Plugins.jacoco)
+    kotlin("jvm") version "1.5.31"
 }
 
 jacoco {
     toolVersion = "0.8.8"
+}
+
+val qodanaToken: String by project
+val javaVersion: String by project
+
+tasks {
+    val build by getting {
+        // Configuração para armazenar em cache as dependências Gradle
+        outputs.upToDateWhen { false }
+        doLast {
+            // Executa a tarefa de build normalmente
+            exec {
+                commandLine = listOf("./gradlew", "build")
+            }
+        }
+    }
+
+    val qodana by registering {
+        dependsOn("build") // Garante que a tarefa de build seja concluída antes de executar o Qodana Scan
+        doLast {
+            // Executa a tarefa de Qodana Scan
+            exec {
+                commandLine = listOf("comando_para_executar_o_Qodana_Scan") // Substitua pelo comando real
+            }
+        }
+    }
+}
+
+// Configuração para cache de dependências Gradle
+
+@Suppress("UnstableApiUsage")
+configurations {
+    create("gradleCache")
+}
+
+dependencies {
+    "gradleCache"("com.github.ben-manes.gradleplugins:gradle-versions-plugin:0.39.0")
+}
+
+tasks.register("restoreCache") {
+    dependsOn("assemble")
+    doLast {
+        exec {
+            commandLine = listOf("gradle", "dependencies", "--configuration", "gradleCache")
+        }
+    }
+}
+
+tasks.register("saveCache") {
+    dependsOn("assemble")
+    doLast {
+        exec {
+            commandLine = listOf("gradle", "dependencies", "--configuration", "gradleCache", "--write-to-file", "gradle_cache.txt")
+        }
+    }
+}
+
+tasks.getByName("build") {
+    dependsOn("restoreCache")
+    finalizedBy("saveCache")
 }
 
 tasks.jacocoTestReport<JacocoReport> {

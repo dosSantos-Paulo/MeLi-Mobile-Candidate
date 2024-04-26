35874e920c827a1bed90b13940f658c64d0293f8
Paulo dos Santos <paulo.dossantos@hotmail.com>
2024-04-26 14:34:09 -0300

    fix: clean MainActivity

--- app/src/main/java/com/dossantos/melimobilecandidate/MainActivity.kt
+++ app/src/main/java/com/dossantos/melimobilecandidate/MainActivity.kt
@@ -3,10 +3,6 @@ package com.dossantos.melimobilecandidate
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dossantos.melimobilecandidate.databinding.ActivityMainBinding
-import com.google.firebase.Firebase
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.analytics
-import com.google.firebase.analytics.logEvent
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -14,21 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val analytics = Firebase.analytics
-        BadCode().complexMethod("Blaclaclaclalcal")
-
-
-
-        binding.sendClick.setOnClickListener {
-            analytics.logEvent("click") {
-                param(FirebaseAnalytics.Param.ITEM_ID, "buttom_home")
-                param(FirebaseAnalytics.Param.ITEM_NAME, "new_button")
-                param(FirebaseAnalytics.Param.CONTENT_TYPE, "buttom")
-            }
-        }
-
-        binding.sendError.setOnClickListener {
-            throw RuntimeException("Enviando erro para crashlytics")
-        }
     }
 }

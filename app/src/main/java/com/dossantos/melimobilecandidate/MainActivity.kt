d91c691b57f6e04b17b6be094e7ae151b78d6391
Paulo dos Santos <paulo.dossantos@hotmail.com>
2024-04-26 14:37:04 -0300

    workflow test: try

--- app/src/main/java/com/dossantos/melimobilecandidate/MainActivity.kt
+++ app/src/main/java/com/dossantos/melimobilecandidate/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dossantos.melimobilecandidate
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dossantos.melimobilecandidate.databinding.ActivityMainBinding
+//Only to run new ci workflow
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }

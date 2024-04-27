8936986136b618a380a910ca4c5e6843318e421b
Paulo dos Santos <paulo.dossantos@hotmail.com>
2024-04-26 23:19:16 -0300

    new-feature: implement Qodana

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,12 @@ plugins {
     id(Plugins.androidApplication) version Versions.Core.agp apply false
     id(Plugins.kotlinAndroid) version Versions.Core.kotlin apply false
     id(Plugins.googleServices) version Versions.Google.googleServices apply false
+    `java-library`
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+    withSourcesJar()
 }

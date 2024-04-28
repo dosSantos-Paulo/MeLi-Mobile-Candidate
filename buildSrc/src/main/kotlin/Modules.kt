01a891aaa2fc29effe22e7571497390acdf2e37d
Paulo dos Santos <paulo.dossantos@hotmail.com>
2024-04-27 23:53:21 -0300

    new-feature: create domain

---
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -0,0 +1,5 @@
+object Modules {
+    const val designSystem = ":designSystem"
+    const val domain = ":domain"
+    const val data = ":data"
+}
---
+++ domain/src/main/java/com/dossantos/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.dossantos.domain
+
+class MyClass {
+}

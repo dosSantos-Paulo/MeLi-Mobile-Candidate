682dd8075abd2a9865467dbdc4e5422d1c8bf25f
Paulo dos Santos <paulo.dossantos@hotmail.com>
2024-04-29 13:15:30 -0300

    new-feature: update KoinModulesWrapper

--- app/src/main/java/com/dossantos/melimobilecandidate/di/KoinModulesWrapper.kt
+++ app/src/main/java/com/dossantos/melimobilecandidate/di/KoinModulesWrapper.kt
@@ -1,6 +1,10 @@
 package com.dossantos.melimobilecandidate.di
 
+import com.dossantos.data.offer.OfferRepositoryImpl
+import com.dossantos.domain.offer.OfferRepository
+import com.dossantos.domain.offer.OfferUseCase
 import com.dossantos.melimobilecandidate.ui.home.HomeViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
@@ -8,9 +12,17 @@ import org.koin.dsl.module
 fun getMainModules() = mainModules
 
 private val mainModules by lazy {
-    loadKoinModules(listOf(viewModelModule))
+    loadKoinModules(listOf(repositoryModule, useCaseModules, viewModelModule))
 }
 
 private val viewModelModule = module {
-    viewModel { HomeViewModel() }
+    viewModel { HomeViewModel(offerUseCase = get()) }
+}
+
+private val useCaseModules = module {
+    single { OfferUseCase(offerRepository = get()) }
+}
+
+private val repositoryModule = module {
+    single<OfferRepository> { OfferRepositoryImpl() }
 }
--- domain/src/main/java/com/dossantos/domain/di/KoinModulesWrapper.kt
+++
@@ -1,9 +0,0 @@
-package com.dossantos.domain.di
-
-import org.koin.core.context.loadKoinModules
-
-fun getDomainModules() = domainModules
-
-private val domainModules by lazy {
-    loadKoinModules(listOf())
-}

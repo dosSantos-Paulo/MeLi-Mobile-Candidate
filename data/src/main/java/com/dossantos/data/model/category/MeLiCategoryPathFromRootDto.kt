723c0a670aac005bce404b07a5e947cbfa29de42
Paulo dos Santos <paulo.dossantos@hotmail.com>
2024-04-30 04:14:30 -0300

    improvement: clear dtos

--- data/src/main/java/com/dossantos/data/model/category/MeLiCategoryPathFromRootDto.kt
+++
@@ -1,6 +0,0 @@
-package com.dossantos.data.model.category
-
-data class MeLiCategoryPathFromRootDto(
-    val id: String,
-    val name: String
-)
--- data/src/main/java/com/dossantos/data/model/info/MeLiInfoCurrencyDto.kt
+++
@@ -1,6 +0,0 @@
-package com.dossantos.data.model.info
-
-data class MeLiInfoCurrencyDto(
-    val id: String,
-    val symbol: String
-)

0e888109f133574678215ac0c6120d035826a1b1
Paulo dos Santos <paulo.dossantos@hotmail.com>
2024-05-01 12:18:18 -0300

    improvement: update helpers

--- app/src/main/java/com/dossantos/melimobilecandidate/di/KoinModulesWrapper.kt
+++ app/src/main/java/com/dossantos/melimobilecandidate/di/KoinModulesWrapper.kt
@@ -5,20 +5,25 @@ import com.dossantos.data.database.MeLiSuggestionDataBase
 import com.dossantos.data.database.MeLiSuggestionsDao
 import com.dossantos.data.network.category.MeLiCategoryEndpoint
 import com.dossantos.data.network.info.MeLiBrInfoEndpoint
+import com.dossantos.data.network.product.MeLiProductEndpoint
 import com.dossantos.data.network.search.MeLiSearchEndpoint
 import com.dossantos.data.repository.category.CategoryRepositoryImpl
 import com.dossantos.data.repository.offer.OfferRepositoryImpl
+import com.dossantos.data.repository.product.ProductRepositoryImpl
 import com.dossantos.data.repository.search.SearchRepositoryImpl
 import com.dossantos.data.repository.suggestions.SuggestionsRepositoryImpl
 import com.dossantos.domain.repository.category.CategoryRepository
 import com.dossantos.domain.repository.offer.OfferRepository
+import com.dossantos.domain.repository.product.ProductRepository
 import com.dossantos.domain.repository.search.SearchRepository
 import com.dossantos.domain.repository.suggestions.SuggestionsRepository
 import com.dossantos.domain.usecase.category.CategoryMenuUseCase
 import com.dossantos.domain.usecase.offer.OfferUseCase
+import com.dossantos.domain.usecase.products.ProductsUseCase
 import com.dossantos.domain.usecase.search.SearchUseCase
 import com.dossantos.domain.usecase.suggestions.SuggestionsUseCase
 import com.dossantos.melimobilecandidate.viewmodel.home.HomeViewModel
+import com.dossantos.melimobilecandidate.viewmodel.product.ProductDetailViewModel
 import com.dossantos.melimobilecandidate.viewmodel.search.SearchViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -40,9 +45,16 @@ private val viewModelModules = module {
         )
     }
 
-    viewModel { parameters ->
+    viewModel {
         SearchViewModel(
             searchUseCase = get()
+        )
+    }
+
+    viewModel { params ->
+        ProductDetailViewModel(
+            productId = params.get<String>(),
+            productsUseCase = get()
         )
     }
 }
@@ -52,6 +64,7 @@ private val useCaseModules = module {
     single { CategoryMenuUseCase(categoryRepository = get()) }
     single { SuggestionsUseCase(suggestionsRepository = get()) }
     single { SearchUseCase(searchRepository = get()) }
+    single { ProductsUseCase(productRepository = get(), suggestionsRepository = get()) }
 }
 
 private val repositoryModules = module {
@@ -72,6 +85,8 @@ private val repositoryModules = module {
     }
 
     single<SearchRepository> { SearchRepositoryImpl(searchEndpoint = MeLiSearchEndpoint.instance) }
+
+    single<ProductRepository> { ProductRepositoryImpl(productEndpoint = MeLiProductEndpoint.instance) }
 }
 
 private val extraModules = module {

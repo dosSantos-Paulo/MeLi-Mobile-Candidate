bb05b94f793444ef49cde2f0dcf230d1bc31c6ba
Paulo dos Santos <paulo.dossantos@hotmail.com>
2024-05-01 02:32:19 -0300

    improvement: update reference to get firebase key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -152,7 +152,7 @@ fun updateFirebaseApi(key: String) {
 
     jsonFile.writeText(
         jsonContent.replace(
-            "\"current_key\": \"wrong_api\"",
+            "\"current_key\": \"wrong_key\"",
             "\"current_key\": $key"
         )
     )

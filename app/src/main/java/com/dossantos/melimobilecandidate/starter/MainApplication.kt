cd257731e78da390abe7a9e1b47379d042794284
Paulo dos Santos <paulo.dossantos@hotmail.com>
2024-05-16 23:42:49 -0300

    start timber

--- app/src/main/java/com/dossantos/melimobilecandidate/starter/MainApplication.kt
+++ app/src/main/java/com/dossantos/melimobilecandidate/starter/MainApplication.kt
@@ -1,16 +1,19 @@
 package com.dossantos.melimobilecandidate.starter
 
 import android.app.Application
+import com.dossantos.melimobilecandidate.BuildConfig
 import com.dossantos.melimobilecandidate.di.getMainModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class MainApplication : Application()  {
 
     override fun onCreate() {
         super.onCreate()
         setupKoin()
+        setupTimber()
     }
 
     private fun setupKoin() {
@@ -18,6 +21,12 @@ class MainApplication : Application()  {
             androidLogger()
             androidContext(this@MainApplication)
             getMainModules()
+        }
+    }
+
+    private fun setupTimber() {
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
         }
     }
 }
